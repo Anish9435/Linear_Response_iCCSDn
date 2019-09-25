31acb39f6fd86c12218bae33a43760bc56338d81
Anish9435 <anish.saheb41@gmail.com>
2019-09-25 19:23:03 +0530

    iCCSDn LRT

--- excitation_energy.py
+++ excitation_energy.py
@@ -5,12 +5,11 @@ import inp
 import MP2
 import intermediates
 import amplitude
+import amplitude_response
 import cc_symmetrize
 import cc_update
 import main
 import math
-
-##commenting here for git 
 
 ##------Import important values-------##
 
@@ -18,9 +17,10 @@ t1 = main.t1
 t2 = main.t2
 So = main.So
 Sv = main.Sv
-tau = main.tau
+#tau = main.tau
 occ = MP2.occ
 virt = MP2.virt
+nao = MP2.nao
 o_act = inp.o_act
 v_act = inp.v_act
 n_iter = inp.n_iter
@@ -31,10 +31,7 @@ D2 = -1.0*MP2.D2
 Do = -1.0*MP2.Do
 Dv = -1.0*MP2.Dv
 t1_new,t2_new,So_new,Sv_new = davidson.guess_X(occ,virt,o_act,v_act)
-t0 = 1.0
-print t1
 twoelecint_mo = MP2.twoelecint_mo 
-nao = MP2.nao
 
 ##-----------Initialization of Dictionaries for storing the values----------##
 
@@ -64,7 +61,6 @@ B_Y_iuab = np.zeros((1,1))
 
 ##------Start iteration--------##
 for x in range(0,n_iter):
-
 ##-----conditioning with the remainder------##
 
   print ("*********")
@@ -88,18 +84,16 @@ for x in range(0,n_iter):
       B_Y_ijav = np.zeros((r+1,r+1))
       B_Y_iuab = np.zeros((r+1,r+1))
       
-      dict_t1[r] = x_t1 # This should be the proper way, anyway convergence is still hard to achieve even with this. Need to look in to it. 
+      dict_t1[r] = x_t1  
       dict_t2[r] = x_t2
-      dict_So[r] = norm_So
-      dict_Sv[r] = norm_Sv
+      dict_So[r] = x_So
+      dict_Sv[r] = x_Sv
   
 ##------Diagrams and intermediates of coupled cluster theory i.e AX with new t and s---------##
-  
   I_vv, I_oo, Ivvvv, Ioooo, Iovvo, Iovvo_2, Iovov,Iovov_2 = intermediates.initialize()
-
 ##-------------Linear terms of both R_ia and R_ijab--------------##
-  Y_ia = amplitude.singles_response_linear(I_oo,I_vv,dict_t1[r],dict_t2[r])
-  Y_ijab = amplitude.doubles_response_linear(I_oo,I_vv,Ivvvv,Ioooo,Iovvo,Iovvo_2,Iovov,Iovov_2,dict_t1[r],dict_t2[r])
+  Y_ia = amplitude_response.singles_response_linear(I_oo,I_vv,dict_t1[r],dict_t2[r])
+  Y_ijab = amplitude_response.doubles_response_linear(I_oo,I_vv,Ivvvv,Ioooo,Iovvo,Iovvo_2,Iovov,Iovov_2,dict_t1[r],dict_t2[r])
 ##---------------------------------------------------------------##
 ##-------------update of the intermediates and geneation of new intermediates----------##
   I1, I2 = intermediates.R_ia_intermediates(t1)
@@ -108,23 +102,21 @@ for x in range(0,n_iter):
   I_oo_new,I_vv_new,Ioooo_new,Iovvo_new,Iovvo_2_new,Iovov_new = intermediates.update_int_response(dict_t2[r],I_vv,I_oo,Ioooo,Iovvo,Iovvo_2,Iovov)
 ##-----------------------------------------------------------------##
 ##--------------one body diagrams---------------------------------##
-  Y_ia += amplitude.singles_response_quadratic(I_oo,I_vv,I1,I2,dict_t1[r],dict_t2[r])
-  Y_ia += amplitude.singles_response_quadratic(I_oo_new,I_vv_new,I1_new,I2_new,t1,t2)
+  Y_ia += amplitude_response.singles_response_quadratic(I_oo,I_vv,I1,I2,dict_t1[r],dict_t2[r])
+  Y_ia += amplitude_response.singles_response_quadratic(I_oo_new,I_vv_new,I1_new,I2_new,t1,t2)
 
-  Y_ia += -2*np.einsum('ibkj,ka,jb->ia',twoelecint_mo[:occ,occ:nao,:occ,:occ],t1,dict_t1[r])
-  Y_ia +=  2*np.einsum('cbaj,ic,jb->ia',twoelecint_mo[occ:nao,occ:nao,occ:nao,:occ],t1,dict_t1[r])
-  Y_ia +=  np.einsum('ibjk,ka,jb->ia',twoelecint_mo[:occ,occ:nao,:occ,:occ],t1,dict_t1[r])
-  Y_ia += -np.einsum('cbja,ic,jb->ia',twoelecint_mo[occ:nao,occ:nao,:occ,occ:nao],t1,dict_t1[r])
+  Y_ia += -2*np.einsum('ibkj,ka,jb->ia',twoelecint_mo[:occ,occ:nao,:occ,:occ],t1,dict_t1[r])       #diagram non-linear a
+  Y_ia +=  2*np.einsum('cbaj,ic,jb->ia',twoelecint_mo[occ:nao,occ:nao,occ:nao,:occ],t1,dict_t1[r]) #diagram non-linear b
+  Y_ia +=  np.einsum('ibjk,ka,jb->ia',twoelecint_mo[:occ,occ:nao,:occ,:occ],t1,dict_t1[r])         #diagram non-linear c
+  Y_ia += -np.einsum('cbja,ic,jb->ia',twoelecint_mo[occ:nao,occ:nao,:occ,occ:nao],t1,dict_t1[r])   #diagram non-linear d
   
   Y_ia += -2*np.einsum('ibkj,ka,jb->ia',twoelecint_mo[:occ,occ:nao,:occ,:occ],dict_t1[r],t1)
   Y_ia +=  2*np.einsum('cbaj,ic,jb->ia',twoelecint_mo[occ:nao,occ:nao,occ:nao,:occ],dict_t1[r],t1)
   Y_ia +=  np.einsum('ibjk,ka,jb->ia',twoelecint_mo[:occ,occ:nao,:occ,:occ],dict_t1[r],t1)
   Y_ia += -np.einsum('cbja,ic,jb->ia',twoelecint_mo[occ:nao,occ:nao,:occ,occ:nao],dict_t1[r],t1)
 ##--------------------------------------------------------------------##
-
   I_oo,I_vv,I_oovo,I_vovv,Ioooo_2,I_voov,Iovov_3,Iovvo_3,Iooov,I3 = intermediates.singles_intermediates(t1,t2,I_oo,I_vv,I2)
   I_oo_new,I_vv_new,I_oovo_new,I_vovv_new,Ioooo_2_new,I_voov_new,Iovov_3_new,Iovvo_3_new,Iooov_new,I3_new = intermediates.singles_intermediates(dict_t1[r],dict_t2[r],I_oo_new,I_vv_new,I2_new)
-
 ##----------CC triple excitation diagram intermediate---------##
   II_oo = intermediates.W1_int_So(So)
   II_oo_new = intermediates.W1_int_So(dict_So[r])
@@ -132,34 +124,33 @@ for x in range(0,n_iter):
   II_vv_new = intermediates.W1_int_Sv(dict_Sv[r])
 ##------------------------------------------------------------##
 ##-----------------Two body diagrams--------------------------##
-  Y_ijab += amplitude.doubles_response_quadratic(I_oo,I_vv,Ioooo,Iovvo,Iovvo_2,Iovov,dict_t2[r]) 
-  Y_ijab += amplitude.doubles_response_quadratic(I_oo_new,I_vv_new,Ioooo_new,Iovvo_new,Iovvo_2_new,Iovov_new,t2) 
-  Y_ijab += amplitude.singles_n_doubles_response(dict_t1[r],I_oovo,I_vovv)
-  Y_ijab += amplitude.singles_n_doubles_response(t1,I_oovo_new,I_vovv_new)
+  Y_ijab += amplitude_response.doubles_response_quadratic(I_oo,I_vv,Ioooo,Iovvo,Iovvo_2,Iovov,dict_t2[r]) 
+  Y_ijab += amplitude_response.doubles_response_quadratic(I_oo_new,I_vv_new,Ioooo_new,Iovvo_new,Iovvo_2_new,Iovov_new,t2) 
+  Y_ijab += amplitude_response.singles_n_doubles_response(dict_t1[r],I_oovo,I_vovv)
+  Y_ijab += amplitude_response.singles_n_doubles_response(t1,I_oovo_new,I_vovv_new)
 
+  Y_ijab += 0.5*np.einsum('ijkl,ka,lb->ijab',twoelecint_mo[:occ,:occ,:occ,:occ],t1,dict_t1[r])      #diagram non-linear 1
+  Y_ijab += 0.5*np.einsum('cdab,ic,jd->ijab',twoelecint_mo[occ:nao,occ:nao,occ:nao,occ:nao],t1,dict_t1[r])  #diagram non-linear 2
   Y_ijab += -np.einsum('ickb,ka,jc->ijab',twoelecint_mo[:occ,occ:nao,:occ,occ:nao],t1,dict_t1[r])   #diagrams non-linear 3
   Y_ijab += -np.einsum('icak,jc,kb->ijab',twoelecint_mo[:occ,occ:nao,occ:nao,:occ],t1,dict_t1[r])   #diagrams non-linear 4
-  Y_ijab += 0.5*np.einsum('ijkl,ka,lb->ijab',twoelecint_mo[:occ,:occ,:occ,:occ],t1,dict_t1[r])      #diagram non-linear 1
-  Y_ijab += 0.5*np.einsum('cdab,ic,jd->ijab',twoelecint_mo[occ:nao,occ:nao,occ:nao,occ:nao],t1,dict_t1[r])      #diagram non-linear 2
   
+  Y_ijab += 0.5*np.einsum('ijkl,ka,lb->ijab',twoelecint_mo[:occ,:occ,:occ,:occ],dict_t1[r],t1)      #diagram non-linear 1
+  Y_ijab += 0.5*np.einsum('cdab,ic,jd->ijab',twoelecint_mo[occ:nao,occ:nao,occ:nao,occ:nao],dict_t1[r],t1)  #diagram non-linear 2
   Y_ijab += -np.einsum('ickb,ka,jc->ijab',twoelecint_mo[:occ,occ:nao,:occ,occ:nao],dict_t1[r],t1)   #diagrams non-linear 3
   Y_ijab += -np.einsum('icak,jc,kb->ijab',twoelecint_mo[:occ,occ:nao,occ:nao,:occ],dict_t1[r],t1)   #diagrams non-linear 4
-  Y_ijab += 0.5*np.einsum('ijkl,ka,lb->ijab',twoelecint_mo[:occ,:occ,:occ,:occ],dict_t1[r],t1)      #diagram non-linear 1
-  Y_ijab += 0.5*np.einsum('cdab,ic,jd->ijab',twoelecint_mo[occ:nao,occ:nao,occ:nao,occ:nao],dict_t1[r],t1)      #diagram non-linear 2
 
-  Y_ijab += amplitude.inserted_diag_So(dict_t2[r],II_oo)
-  Y_ijab += amplitude.inserted_diag_So(t2,II_oo_new)
-  Y_ijab += amplitude.inserted_diag_Sv(dict_t2[r],II_vv)
-  Y_ijab += amplitude.inserted_diag_Sv(t2,II_vv)
+  Y_ijab += amplitude_response.inserted_diag_So(dict_t2[r],II_oo)
+  Y_ijab += amplitude_response.inserted_diag_So(t2,II_oo_new)
+  Y_ijab += amplitude_response.inserted_diag_Sv(dict_t2[r],II_vv)
+  Y_ijab += amplitude_response.inserted_diag_Sv(t2,II_vv)
   Y_ijab = cc_symmetrize.symmetrize(Y_ijab)
 ##------------------------------------------------------------##
 ##----------Diagram contributing to R_ijav and R_iuab------------## 
-  Y_iuab = amplitude.Sv_diagram_vs_contraction(dict_Sv[r],II_vv)
-  Y_iuab += amplitude.Sv_diagram_vt_contraction(dict_t2[r])
-  Y_ijav = amplitude.So_diagram_vs_contraction(dict_So[r],II_oo)
-  Y_ijav += amplitude.So_diagram_vt_contraction(dict_t2[r])
+  Y_iuab = amplitude_response.Sv_diagram_vs_contraction_response(dict_Sv[r],II_vv)
+  Y_iuab += amplitude_response.Sv_diagram_vt_contraction_response(dict_t2[r])
+  Y_ijav = amplitude_response.So_diagram_vs_contraction_response(dict_So[r],II_oo)
+  Y_ijav += amplitude_response.So_diagram_vt_contraction_response(dict_t2[r])
 ##-------------------------------------------------------------------------------------------##
-
 ##-------Storing AX in the dictionary---------##
   
   dict_Y_ia[r] = Y_ia
@@ -200,13 +191,13 @@ for x in range(0,n_iter):
     B_Y_ijav[m-1,r] = 2.0*np.einsum('ijav,ijav',dict_So[m-1],dict_Y_ijav[r])-np.einsum('jiav,ijav',dict_So[m-1],dict_Y_ijav[r])
     B_Y_ijav[r,m-1] = 2.0*np.einsum('ijav,ijav',dict_So[r],dict_Y_ijav[m-1])-np.einsum('jiav,ijav',dict_So[r],dict_Y_ijav[m-1])
 
-    B_Y_iuab[m-1,r] = -2.0*np.einsum('iuab,iuab',dict_Sv[m-1],dict_Y_iuab[r])+np.einsum('iuba,iuab',dict_Sv[m-1],dict_Y_iuab[r])
-    B_Y_iuab[r,m-1] = -2.0*np.einsum('iuab,iuab',dict_Sv[r],dict_Y_iuab[m-1])+np.einsum('iuba,iuab',dict_Sv[r],dict_Y_iuab[m-1])
+    B_Y_iuab[m-1,r] = 2.0*np.einsum('iuab,iuab',dict_Sv[m-1],dict_Y_iuab[r])-np.einsum('iuba,iuab',dict_Sv[m-1],dict_Y_iuab[r])
+    B_Y_iuab[r,m-1] = 2.0*np.einsum('iuab,iuab',dict_Sv[r],dict_Y_iuab[m-1])-np.einsum('iuba,iuab',dict_Sv[r],dict_Y_iuab[m-1])
 
   B_Y_ia[r,r] = 2.0*np.einsum('ia,ia',dict_t1[r],dict_Y_ia[r])
   B_Y_ijab[r,r] = 2.0*np.einsum('ijab,ijab',dict_t2[r],dict_Y_ijab[r])-np.einsum('ijba,ijab',dict_t2[r],dict_Y_ijab[r])
   B_Y_ijav[r,r] = 2.0*np.einsum('ijav,ijav',dict_So[r],dict_Y_ijav[r])-np.einsum('jiav,ijav',dict_So[r],dict_Y_ijav[r])
-  B_Y_iuab[r,r] = -2.0*np.einsum('iuab,iuab',dict_Sv[r],dict_Y_iuab[r])+np.einsum('iuba,iuab',dict_Sv[r],dict_Y_iuab[r])
+  B_Y_iuab[r,r] = 2.0*np.einsum('iuab,iuab',dict_Sv[r],dict_Y_iuab[r])-np.einsum('iuba,iuab',dict_Sv[r],dict_Y_iuab[r])
    
   B_total = B_Y_ia+B_Y_ijab+B_Y_ijav+B_Y_iuab
   #print B_total
@@ -252,7 +243,7 @@ for x in range(0,n_iter):
   lin_norm = 2.0*np.einsum('ia,ia',x_t1,x_t1)
   lin_norm += 2.0*np.einsum('ijab,ijab',x_t2,x_t2)-np.einsum('ijab,ijba',x_t2,x_t2)
   lin_norm += 2.0*np.einsum('ijav,ijav',x_So,x_So)-np.einsum('ijav,jiav',x_So,x_So)
-  lin_norm += -2.0*np.einsum('iuab,iuba',x_Sv,x_Sv)+np.einsum('iuab,iuba',x_Sv,x_Sv)
+  lin_norm += 2.0*np.einsum('iuab,iuba',x_Sv,x_Sv)-np.einsum('iuab,iuba',x_Sv,x_Sv)
   norm = math.sqrt(lin_norm)
  
   if (norm > 1e-9):
@@ -281,7 +272,6 @@ for x in range(0,n_iter):
   print 'EPS: ', eps_t 
   print 'Eigen value: ', w_total[ind_min_wtotal]
   if (eps_t <= conv and eps_So <= conv and eps_Sv <= conv):
-  #if (eps_t <= conv):
     print "CONVERGED!!!!!!!!!!!!"
     print 'Excitation Energy: ', w_total[ind_min_wtotal]
     break
@@ -302,25 +292,27 @@ for x in range(0,n_iter):
   ortho_Sv = Sv_2
   
   for i in range(0,r+1):
-    ovrlap = 2.0*np.einsum('ia,ia',t1_2,dict_t1[i]) + 2.0*np.einsum('ijab,ijab',t2_2,dict_t2[i]) - np.einsum('ijab,ijba',t2_2,dict_t2[i]) + 2.0*np.einsum('ijav,ijav',So_2,dict_So[r]) - np.einsum('ijav,jiav',So_2,dict_So[r]) - 2.0*np.einsum('iuab,iuab',Sv_2,dict_Sv[r]) + np.einsum('iuab,iuba',Sv_2,dict_Sv[r])
-    ortho_t1 += - ovrlap*dict_t1[i]  
-    ortho_t2 += - ovrlap*dict_t2[i]  
-    ortho_So += - ovrlap*dict_So[i]  
-    ortho_Sv += - ovrlap*dict_Sv[i]  
-    #X = np.einsum('ia,ia',ortho_t1,dict_t1[i])
-    #Y = np.einsum('ijab,ijab',ortho_t2,dict_t2[i])
-    #print X, Y 
+    nrm = 2.0*np.einsum('ia,ia',dict_t1[i],dict_t1[i]) + 2.0*np.einsum('ijab,ijab',dict_t2[i],dict_t2[i]) - np.einsum('ijab,ijba',dict_t2[i],dict_t2[i]) + 2.0*np.einsum('ijav,ijav',dict_So[i],dict_So[i]) - np.einsum('ijav,jiav',dict_So[i],dict_So[i]) + 2.0*np.einsum('iuab,iuab',dict_Sv[i],dict_Sv[i]) - np.einsum('iuab,iuba',dict_Sv[i],dict_Sv[i])
+
+    ovrlap = 2.0*np.einsum('ia,ia',t1_2,dict_t1[i]) + 2.0*np.einsum('ijab,ijab',t2_2,dict_t2[i]) - np.einsum('ijab,ijba',t2_2,dict_t2[i]) + 2.0*np.einsum('ijav,ijav',So_2,dict_So[r]) - np.einsum('ijav,jiav',So_2,dict_So[r]) + 2.0*np.einsum('iuab,iuab',Sv_2,dict_Sv[r]) - np.einsum('iuab,iuba',Sv_2,dict_Sv[r])
+
+    ortho_t1 += -(ovrlap*dict_t1[i])/nrm  
+    ortho_t2 += -(ovrlap*dict_t2[i])/nrm  
+    ortho_So += -(ovrlap*dict_So[i])/nrm  
+    ortho_Sv += -(ovrlap*dict_Sv[i])/nrm  
   #for i in range(0,r+1):
-  #  p = 2.0*np.einsum('ia,ia',dict_t1[i],ortho_t1)
-  #  q = 2.0*np.einsum('ijab,ijab',dict_t2[i],ortho_t2)-np.einsum('ijab,ijba',dict_t2[i],ortho_t2)
-  #  y = p+q
-  #  print "overlap:", i,p,q,y  
-  
+   # p = 2.0*np.einsum('ia,ia',dict_t1[i],ortho_t1)
+   # q = 2.0*np.einsum('ijab,ijab',dict_t2[i],ortho_t2)-np.einsum('ijab,ijba',dict_t2[i],ortho_t2)
+   # t = 2.0*np.einsum('ijav,ijav',dict_So[i],ortho_So)-np.einsum('ijab,jiav',dict_So[i],ortho_So)
+   # d = 2.0*np.einsum('iuab,iuab',dict_Sv[i],ortho_Sv)-np.einsum('iuab,iuba',dict_Sv[i],ortho_Sv)
+   # y = p+q+t+d
+   # print "overlap:", i,p,q,t,d,y  
+ 
 ##--------Normalization of the new t and s------##
 
   ortho_norm = 2.0*np.einsum('ia,ia',ortho_t1,ortho_t1)
   ortho_norm += 2.0*np.einsum('ijab,ijab',ortho_t2,ortho_t2)-np.einsum('ijab,ijba',ortho_t2,ortho_t2)
-  ortho_norm += -2.0*np.einsum('ijav,ijav',ortho_So,ortho_So)+np.einsum('ijav,jiav',ortho_So,ortho_So)
+  ortho_norm += 2.0*np.einsum('ijav,ijav',ortho_So,ortho_So)-np.einsum('ijav,jiav',ortho_So,ortho_So)
   ortho_norm += 2.0*np.einsum('iuab,iuba',ortho_Sv,ortho_Sv)-np.einsum('iuab,iuba',ortho_Sv,ortho_Sv)
   norm_total = math.sqrt(ortho_norm)
 
@@ -340,6 +332,6 @@ for x in range(0,n_iter):
   dict_t2[r+1] = norm_t2
   dict_So[r+1] = norm_So
   dict_Sv[r+1] = norm_Sv
-  nrm = 2.0*np.einsum('ia,ia',dict_t1[r+1],dict_t1[r+1]) + 2.0*np.einsum('ijab,ijab',dict_t2[r+1],dict_t2[r+1]) - np.einsum('ijab,ijba',dict_t2[r+1],dict_t2[r+1]) + 2.0*np.einsum('ijav,ijav',dict_So[r+1],dict_So[r+1]) - np.einsum('ijav,jiav',dict_So[r+1],dict_So[r+1]) - 2.0*np.einsum('iuab,iuab',dict_Sv[r+1],dict_Sv[r+1]) + np.einsum('iuab,iuba',dict_Sv[r+1],dict_Sv[r+1])
+  nrm = 2.0*np.einsum('ia,ia',dict_t1[r+1],dict_t1[r+1]) + 2.0*np.einsum('ijab,ijab',dict_t2[r+1],dict_t2[r+1]) - np.einsum('ijab,ijba',dict_t2[r+1],dict_t2[r+1]) + 2.0*np.einsum('ijav,ijav',dict_So[r+1],dict_So[r+1]) - np.einsum('ijav,jiav',dict_So[r+1],dict_So[r+1]) + 2.0*np.einsum('iuab,iuab',dict_Sv[r+1],dict_Sv[r+1]) - np.einsum('iuab,iuba',dict_Sv[r+1],dict_Sv[r+1])
   print "final norm:", nrm 
      
